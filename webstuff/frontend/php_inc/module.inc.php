89c7264674f8578db408941c34d0ff75af1b949d
losinshi <losinshi@dd492736-c11a-0410-ad51-8c26713eaf7f>
2006-08-16 01:48:49 +0000

    Fix a little syntax failture. god night!
    git-svn-id: file:///home/jan/tmp/wetterstation/trunk@22 dd492736-c11a-0410-ad51-8c26713eaf7f

--- webstuff/frontend/php_inc/module.inc.php
+++ webstuff/frontend/php_inc/module.inc.php
@@ -1,8 +1,8 @@
 <?
 class Module{
 
-  var parserInstance     = NULL;	/* Parser - Instanz */
-  var connectionInstance = NULL;	/* Connection - Instanz */
+  var $parserInstance     = NULL;	/* Parser - Instanz */
+  var $connectionInstance = NULL;	/* Connection - Instanz */
 
   function Module($modName, $sensId, $parser, $connection){
 
