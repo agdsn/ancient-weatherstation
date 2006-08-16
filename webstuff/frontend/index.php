d221d9cda8e4337a634fe2032e246e1e5eebbb4c
losinshi <losinshi@dd492736-c11a-0410-ad51-8c26713eaf7f>
2006-08-16 01:42:17 +0000

    second Part of initial checkin
    git-svn-id: file:///home/jan/tmp/wetterstation/trunk@21 dd492736-c11a-0410-ad51-8c26713eaf7f

---
+++ webstuff/frontend/index.php
@@ -0,0 +1,14 @@
+<?
+  include_once("php_inc/module_set.inc.php");
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+  <head>
+    <title>Wetter-Testfile</title>
+    <link rel="styleshee-" type="text/css" href="css/main1.css">
+  </head>
+  <body>
+    <h1>Wetter-Testfile</h1>
+	<? new ModuleSet($_REQUEST['setType']);?>
+  </body>
+</html>
