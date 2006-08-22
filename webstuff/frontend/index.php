a200071abaee63f4cb6fda94578d2029bcebb804
losinshi <losinshi@dd492736-c11a-0410-ad51-8c26713eaf7f>
2006-08-22 22:56:04 +0000

    Make the output fron frontend more readable
    git-svn-id: file:///home/jan/tmp/wetterstation/trunk@39 dd492736-c11a-0410-ad51-8c26713eaf7f

--- webstuff/frontend/index.php
+++ webstuff/frontend/index.php
@@ -4,11 +4,11 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
   <head>
-    <title>Wetter-Testfile</title>
+    <title>Wetterstations-Testanzeige</title>
     <link rel="stylesheet" type="text/css" href="css/example.css">
   </head>
   <body>
-    <h1>Wetter-Testfile</h1>
+    <h1>Wetterstations-Testanzeige</h1>
 	<? new ModuleSet($_REQUEST['setType']);?>
   </body>
 </html>
