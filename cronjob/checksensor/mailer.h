d50e20977b2fab71ee6ef5d44d29527ebafe610a
losinshi <losinshi@dd492736-c11a-0410-ad51-8c26713eaf7f>
2006-08-26 01:50:27 +0000

    License information added to the mailer.c and mailer.h
    git-svn-id: file:///home/jan/tmp/wetterstation/trunk@46 dd492736-c11a-0410-ad51-8c26713eaf7f

--- cronjob/checksensor/mailer.h
+++ cronjob/checksensor/mailer.h
@@ -1,3 +1,31 @@
+/*
+   mailer.h      -- Wrapper for libesmtp
+
+   Copyright (C) 2006 Jan Losinski
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+/* Compiling:
+ * it uses libesmtp and libssl, so you must add
+ * -lesmtp and -lssl for linking.
+ * Debug-output can be activated with -DDEBUG as 
+ * compile-option. */
+
+
 #include <auth-client.h>
 
 #define SSL_ENABLED   0
