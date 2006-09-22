a1048194ebab83ecefde74c73de86e4ea7f9ce5b
losinshi <losinshi@dd492736-c11a-0410-ad51-8c26713eaf7f>
2006-09-22 09:50:59 +0000

    bugfixes and descriptions
    git-svn-id: file:///home/jan/tmp/wetterstation/trunk@126 dd492736-c11a-0410-ad51-8c26713eaf7f

--- webstuff/frontend/index.php
+++ webstuff/frontend/index.php
@@ -4,11 +4,11 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
   <head>
-    <title>Wetterstations-Testanzeige</title>
+    <title>Meine Wetterstation</title>
     <link rel="stylesheet" type="text/css" href="css/example.css">
   </head>
   <body>
-    <h1>Wetterstations-Testanzeige</h1>
+    <h1 align="center">Meine Wetterstation</h1>
 	<? new ModuleSet($_REQUEST['setType']);?>
   </body>
 </html>
