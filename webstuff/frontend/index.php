b98053c3b80a0b1389df8bd6251f1681c752ae1c
losinshi <losinshi@dd492736-c11a-0410-ad51-8c26713eaf7f>
2006-09-24 18:38:41 +0000

    bugfives
    git-svn-id: file:///home/jan/tmp/wetterstation/trunk@129 dd492736-c11a-0410-ad51-8c26713eaf7f

--- webstuff/frontend/index.php
+++ webstuff/frontend/index.php
@@ -1,5 +1,6 @@
 <?
-  include_once("php_inc/module_set.inc.php");
+  $path = "./";
+  include_once($path."php_inc/module_set.inc.php");
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
