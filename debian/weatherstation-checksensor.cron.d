50407b25a603b7b3f68651ad2d41ce1de55b3775
losinshi <losinshi@dd492736-c11a-0410-ad51-8c26713eaf7f>
2006-09-28 01:06:21 +0000

    cronjob changed
    git-svn-id: file:///home/jan/tmp/wetterstation/trunk@180 dd492736-c11a-0410-ad51-8c26713eaf7f

--- debian/weatherstation-checksensor.cron.d
+++ debian/weatherstation-checksensor.cron.d
@@ -1,4 +1,4 @@
 #
 # Regular cron jobs for the weatherstation package
 #
-0 12 	* * * 	weatherstation	/usr/bin/weather_checksensor
+0 12 	* * * 	weatherstation	/usr/bin/weather_checksensor 2>&1 >/dev/null
